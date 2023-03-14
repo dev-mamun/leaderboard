a8f92cb91f8831e5dfdfaed30435fc67b421b51a
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-03-14 23:58:20 +0600

    Setup project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/** ****************************************
+ * Project: leaderboard
+ * File: index.js
+ * Created: 3/14/23
+ * Author: Abdullah Al Mamun <mamun1214@gmail.com>
+ ****************************************** */
+
+import './css/apps.css';
